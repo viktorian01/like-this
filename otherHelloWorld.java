1844562813c401d094fbf28ca4211dc54565f18d
viktorian01 <126495573+viktorian01@users.noreply.github.com>
2023-03-11 20:30:41 +0200

    Add anything!!

--- otherHelloWorld.java
+++ otherHelloWorld.java
@@ -2,5 +2,7 @@
 public class otherHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 2+3;
+        System.out.println(a);
     }
 }
